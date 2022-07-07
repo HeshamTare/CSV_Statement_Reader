f987126adddeeaa2a3d7b18d7fe821a10a220674
HeshamTare <84232978+HeshamTare@users.noreply.github.com>
2022-07-07 13:15:11 +0100

    Update ReturnStringBetweenIndexes.py

--- Functions/ReturnStringBetweenIndexes.py
+++ Functions/ReturnStringBetweenIndexes.py
@@ -1,32 +1,41 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# function to return the characters as a string between two given spaces
-
-
 def substring_between_letters(word, start, end):  # characters between the 'start' and 'end' characters will be returned
+    """return the characters as a string between two given characters.
+    
+    Args:
+        word: the word to be broken down between the 'start' and 'end' characters.
+        start: the character to start breaking down the word from.
+        end: the character to stop at.
+    
+    Returns:
+        the characters between the specified 'start' and 'end' characters of the entered 'word'
+    """
+    
     start_index = 0
     end_index = 0
-
-    for i in word:  # returns the index of the 'start' character
+    
+    # finds the index of the 'start' character
+    for i in word:  
         if i == start:
             break
         else:
             start_index += 1
-
-    for char in word:  # returns the index of the 'end' character
+    
+    # finds the index of the 'end' character
+    for char in word:  
         if char == end:
             break
         else:
             end_index += 1
 
-    if start_index + 1 == end_index or start_index == end_index:  # checks if the returned index's are the same or if there is only -/+ 1 differnece between the two (meaning no characters will be between the two) and returns None if true
+    # ensures characters exist between the specified 'start' and 'end'        
+    if start_index + 1 == end_index or start_index == end_index:  
         return None
     else:
-
-        # provided there are characters between the 'start' and 'end' index's this will return the characters
-
-        start_index += 1  # add 1 to 'start' index so as not to include it in the returned string
-        if start in word and end in word[start_index:]:  # checks that both 'start' and 'end' characters are in the provided 'word' else it returns the whole word
+        
+        # add 1 to 'start' index to prevent it being in the returned string
+        start_index += 1  
+        # ensures the specified 'start' and 'end' characters are in the provided 'word' else the whole word is returned
+        if start in word and end in word[start_index:]:  
             return str(word[start_index:end_index])
         else:
             return word
