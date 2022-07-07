b673d31d1ce9633bb1df5622caa14b9a66ac0c07
HeshamTare <84232978+HeshamTare@users.noreply.github.com>
2022-07-07 11:46:35 +0100

    Update EvenIndexCharacters.py

--- Functions/EvenIndexCharacters.py
+++ Functions/EvenIndexCharacters.py
@@ -1,16 +1,19 @@
-#function to return every other letter in a string
-def every_other_letter(word):
-    every_other_index = 0 
-    temp_strng = "" #string to store every other letter
+# function to return every other letter in a string
 
-    for i in word: #loops through each letter and checks if its index is even, if so it will be added to the 'temp_strng' variable
-      if every_other_index % 2 > 0:
-        every_other_index += 1
-        continue
-      else:
-        temp_strng += i
-        every_other_index += 1
-    
+
+def every_other_letter(word):
+    every_other_index = 0
+    temp_strng = ''  # string to store every other letter
+
+    for i in word:  # loops through each letter and checks if its index is even, if so it will be added to the 'temp_strng' variable
+        if every_other_index % 2 > 0:
+            every_other_index += 1
+            continue
+        else:
+            temp_strng += i
+            every_other_index += 1
+
     return temp_strng
 
-print(every_other_letter("Hello Hesham"))
+
+print every_other_letter('Hello Hesham')
