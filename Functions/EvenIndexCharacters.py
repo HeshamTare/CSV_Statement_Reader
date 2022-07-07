a462849c4b333baa0159798c8909ab109f059565
HeshamTare <84232978+HeshamTare@users.noreply.github.com>
2022-07-07 12:58:04 +0100

    Update EvenIndexCharacters.py

--- Functions/EvenIndexCharacters.py
+++ Functions/EvenIndexCharacters.py
@@ -1,11 +1,18 @@
-# function to return every other letter in a string
-
-
 def every_other_letter(word):
+    """Removes every other letter in a string.
+    
+    Args:
+        word: the string from which every other letter will be removed.
+        
+    Returns:
+        the remaining letters in the string.
+    """
     every_other_index = 0
-    temp_strng = ''  # string to store every other letter
-
-    for i in word:  # loops through each letter and checks if its index is even, if so it will be added to the 'temp_strng' variable
+    # string to store every other letter
+    temp_strng = ''  
+    
+    # loops through each letter and checks if its index is even, if so it will be added to the 'temp_strng' variable
+    for i in word:  
         if every_other_index % 2 > 0:
             every_other_index += 1
             continue
