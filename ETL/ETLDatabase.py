5b4bafd0d2f421c2b126f89027985f88b9336124
HeshamTare <84232978+HeshamTare@users.noreply.github.com>
2022-07-07 11:52:03 +0100

    Update ETLDatabase.py

--- ETL/ETLDatabase.py
+++ ETL/ETLDatabase.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import mysql.connector  # imports mysql library to allow communication between Python and MySQL database
 from mysql.connector import Error  # imports mysql connector error list
 
