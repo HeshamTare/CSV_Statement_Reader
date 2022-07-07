65187d1884fca680b6cd120f73ebf129c0a5f97f
HeshamTare <84232978+HeshamTare@users.noreply.github.com>
2022-07-07 12:53:29 +0100

    Update SchoolClasses.py

--- Classes/SchoolClasses.py
+++ Classes/SchoolClasses.py
@@ -1,4 +1,7 @@
 class Student:
+    """Contains the name, year and grades of a student.
+    """
+    
     def __init__(self, name, year):
         self.name = name
         self.year = year
@@ -6,12 +9,24 @@ class Student:
 
     def add_grade(
         self, grade
-    ):  # function to append a students grades to their grades list and check if it is a passing grade
+    ): 
+        """Append a grade to a students list of grades and checks if it is a passing grade.
+        
+        Args:
+            grade: must be an integer
+        
+        Returns:
+            A string confirming if the grade is a pass or a fail
+        """
+        
         if type(grade) == Grade:
             self.grades.append(grade.score)
             Grade.is_passing(grade)
 
-    def get_average(self):  # returns a students average grade
+    def get_average(self):
+        """Returns the average grade of a student
+        """
+        
         temp_score = 0
         counter = 0
         for i in self.grades:
@@ -26,13 +41,20 @@ sandro = Student("Sandro Botticelli", 12)
 pieter = Student("Pieter Bruegel the Elder", 8)
 
 
-class Grade:  # Class to govern grades
-    minimum_passing = 65  # the minimum score required to pass
+class Grade:  
+    """Ensures grades entered are a consistent format
+    """
+    
+    # Sets the minimum score required to pass
+    minimum_passing = 65  
 
     def __init__(self, score):
         self.score = score
 
-    def is_passing(self):  # checks if the students entered grade is a passing grade
+    def is_passing(self):  
+        """checks if the students entered grade is a passing grade.
+        """
+        
         if self.score == self.minimum_passing or self.score > self.minimum_passing:
             return print(self.score, "is a passing score.")
         else:
